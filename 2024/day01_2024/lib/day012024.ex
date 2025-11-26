4af8ebf0dd0143159ef2de0cf933f77f71cd65bb
Wes Garrison <wes@databasically.com>
2025-11-26 12:05:47 -0600

    day 01 2024 initial commit

---
+++ 2024/day01_2024/lib/day012024.ex
@@ -0,0 +1,18 @@
+defmodule Day012024 do
+  @moduledoc """
+  Documentation for `Day012024`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day012024.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2024/day01_2024/test/day012024_test.exs
@@ -0,0 +1,8 @@
+defmodule Day012024Test do
+  use ExUnit.Case
+  doctest Day012024
+
+  test "greets the world" do
+    assert Day012024.hello() == :world
+  end
+end

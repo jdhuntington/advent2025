ad213212efd60414ea83e1a2eb78ea54997616e4
Wes Garrison <wes@databasically.com>
2025-11-26 12:38:28 -0600

    flesh out example in test directly

--- 2024/day01_2024/test/day012024_test.exs
+++ 2024/day01_2024/test/day012024_test.exs
@@ -5,4 +5,34 @@ defmodule Day012024Test do
   test "greets the world" do
     assert Day012024.hello() == :world
   end
+
+  test "can read the input" do
+    IO.inspect("")
+
+    {left, right} =
+      "test/fixtures/input01.txt"
+      |> File.read!()
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [a, b] = String.split(line, " ", trim: true)
+        {String.to_integer(a), String.to_integer(b)}
+      end)
+      |> Enum.unzip()
+      |> then(fn {l, r} -> {Enum.sort(l), Enum.sort(r)} end)
+
+    IO.inspect(left, label: " left after loading and sorting")
+    IO.inspect(right, label: "right after loading and sorting")
+
+    diffs =
+      Enum.zip(left, right)
+      |> Enum.map(fn {l, r} -> abs(l - r) end)
+
+    IO.inspect(diffs, label: "diffs")
+    assert diffs == [2, 1, 0, 1, 2, 5]
+    assert Enum.sum(diffs) == 11
+  end
+
+  # test "loads lists"
+
+  # test "calculates differences"
 end

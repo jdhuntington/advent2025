7c7ed7cd6e96d6b78930f529807fef485b1abee3
Wes Garrison <wes@databasically.com>
2025-11-26 15:52:55 -0600

    day 01 step 01 complete (answer 1722302)

--- 2024/day01_2024/lib/day012024.ex
+++ 2024/day01_2024/lib/day012024.ex
@@ -8,11 +8,30 @@ defmodule Day012024 do
 
   ## Examples
 
-      iex> Day012024.hello()
-      :world
-
+      iex> Day012024.process_file("test/fixtures/input01.txt")
   """
-  def hello do
-    :world
+
+  def process_file(filepath) do
+    {left, right} =
+      filepath
+      |> File.read!()
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [a, b] = String.split(line, " ", trim: true)
+        {String.to_integer(a), String.to_integer(b)}
+      end)
+      |> Enum.unzip()
+      |> then(fn {l, r} -> {Enum.sort(l), Enum.sort(r)} end)
+
+    IO.inspect(left, label: " left after loading and sorting")
+    IO.inspect(right, label: "right after loading and sorting")
+
+    differences = calculate_differences(left, right)
+    %{left: left, right: right, differences: differences}
+  end
+
+  def calculate_differences(left, right) do
+    Enum.zip(left, right)
+    |> Enum.map(fn {l, r} -> abs(l - r) end)
   end
 end
--- 2024/day01_2024/test/day012024_test.exs
+++ 2024/day01_2024/test/day012024_test.exs
@@ -1,38 +1,27 @@
 defmodule Day012024Test do
   use ExUnit.Case
-  doctest Day012024
-
-  test "greets the world" do
-    assert Day012024.hello() == :world
-  end
+  # doctest Day012024
 
   test "can read the input" do
     IO.inspect("")
 
-    {left, right} =
-      "test/fixtures/input01.txt"
-      |> File.read!()
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn line ->
-        [a, b] = String.split(line, " ", trim: true)
-        {String.to_integer(a), String.to_integer(b)}
-      end)
-      |> Enum.unzip()
-      |> then(fn {l, r} -> {Enum.sort(l), Enum.sort(r)} end)
+    result = Day012024.process_file("test/fixtures/input01.txt")
 
-    IO.inspect(left, label: " left after loading and sorting")
-    IO.inspect(right, label: "right after loading and sorting")
+    IO.inspect(result.differences, label: "result.differences")
+    assert result.differences == [2, 1, 0, 1, 2, 5]
 
-    diffs =
-      Enum.zip(left, right)
-      |> Enum.map(fn {l, r} -> abs(l - r) end)
-
-    IO.inspect(diffs, label: "diffs")
-    assert diffs == [2, 1, 0, 1, 2, 5]
-    assert Enum.sum(diffs) == 11
+    IO.inspect(Enum.sum(result.differences), label: "Enum.sum(result.differences)")
+    assert Enum.sum(result.differences) == 11
   end
 
-  # test "loads lists"
+  test "process input02" do
+    IO.puts("")
+    result = Day012024.process_file("test/fixtures/input02.txt")
 
-  # test "calculates differences"
+    # IO.inspect(result.differences, label: "result.differences")
+    # assert result.differences == [2, 1, 0, 1, 2, 5]
+
+    IO.inspect(Enum.sum(result.differences), label: "Enum.sum(result.differences)")
+    # assert Enum.sum(result.differences) == 11
+  end
 end

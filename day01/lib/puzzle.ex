0e59cf79d76c7716af86158fa00c958d2a1fd2d1
JD Huntington <jd@itsthehuntingtons.com>
2025-12-01 10:59:27 -0600

    more tests pass

--- day01/lib/puzzle.ex
+++ day01/lib/puzzle.ex
@@ -8,17 +8,19 @@ defmodule Puzzle do
     raise "Part 2 not implemented yet"
   end
 
-  def run(state, inputs), do: Enum.reduce(inputs, state, &transform/2)
+  def run(state, inputs) do
+    Enum.reduce(inputs, state, &transform/2)
+  end
 
   def parse_line("R" <> distance), do: {:right, String.to_integer(distance)}
   def parse_line("L" <> distance), do: {:left, String.to_integer(distance)}
 
-  def transform(state, {:right, distance}) do
+  def transform({:right, distance}, state) do
     transform_priv(state, distance)
   end
 
-  def transform(state, {:left, distance}) do
-    transform_priv(state, 0 - distance)
+  def transform({:left, distance}, state) do
+    transform_priv(state, distance * -1)
   end
 
   defp transform_priv({current, hits}, distance) do
--- day01/test/puzzle_test.exs
+++ day01/test/puzzle_test.exs
@@ -9,42 +9,35 @@ defmodule PuzzleTest do
     assert Puzzle.part1(@sample) == 3
   end
 
-  @tag :pending
   test "parse R2" do
     assert Puzzle.parse_line("R2") == {:right, 2}
   end
 
-  @tag :pending
   test "parse L999" do
     assert Puzzle.parse_line("L999") == {:left, 999}
   end
 
-  @tag :pending
   test "transform simple right" do
-    assert Puzzle.transform({50, 0}, {:right, 2}) == {52, 0}
+    assert Puzzle.transform({:right, 2}, {50, 0}) == {52, 0}
   end
 
-  @tag :pending
   test "transform simple left" do
-    assert Puzzle.transform({50, 0}, {:left, 2}) == {48, 0}
+    assert Puzzle.transform({:left, 2}, {50, 0}) == {48, 0}
   end
 
-  @tag :pending
   test "transform doesn't return negative numbers" do
-    assert Puzzle.transform({0, 0}, {:left, 1}) == {99, 0}
-    assert Puzzle.transform({1, 0}, {:left, 2}) == {99, 0}
-    assert Puzzle.transform({2, 0}, {:left, 4}) == {98, 0}
-    assert Puzzle.transform({98, 0}, {:right, 3}) == {1, 0}
+    assert Puzzle.transform({:left, 1}, {0, 0}) == {99, 0}
+    assert Puzzle.transform({:left, 2}, {1, 0}) == {99, 0}
+    assert Puzzle.transform({:left, 4}, {2, 0}) == {98, 0}
+    assert Puzzle.transform({:right, 3}, {98, 0}) == {1, 0}
   end
 
-  @tag :pending
   test "transform catches hits" do
-    assert Puzzle.transform({0, 0}, {:left, 0}) == {0, 1}
-    assert Puzzle.transform({1, 0}, {:left, 1}) == {0, 1}
-    assert Puzzle.transform({50, 1}, {:right, 50}) == {0, 2}
+    assert Puzzle.transform({:left, 0}, {0, 0}) == {0, 1}
+    assert Puzzle.transform({:left, 1}, {1, 0}) == {0, 1}
+    assert Puzzle.transform({:right, 50}, {50, 1}) == {0, 2}
   end
 
-  @tag :pending
   test "process inputs processes a list correctly" do
     assert Puzzle.run({99, 0}, [{:right, 1}, {:right, 2}, {:left, 1}, {:left, 1}]) ==
              {0, 2}

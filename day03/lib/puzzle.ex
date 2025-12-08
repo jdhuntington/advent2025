455ce26fd55da39043340cbc1047cc6f2b2a6b04
Wes Garrison <wes@databasically.com>
2025-12-07 23:54:18 -0600

    day03 part 2

--- day03/lib/puzzle.ex
+++ day03/lib/puzzle.ex
@@ -1,16 +1,34 @@
 defmodule Puzzle do
   def part1(lines) do
-    lines |> Enum.map(&line_joltage/1) |> Enum.sum()
+    lines |> Enum.map(&part1_line_joltage/1) |> Enum.sum()
   end
 
-  def part2(_lines) do
-    raise "Part 2 not implemented yet"
+  def part2(lines) do
+    lines |> Enum.map(&part2_line_joltage/1) |> Enum.sum()
   end
 
-  def line_joltage(bank) do
+  def part1_line_joltage(bank) do
     {dig_one, pos_one} = highest_digit_and_pos(String.slice(bank, 0..(String.length(bank) - 2)))
     {dig_two, _} = highest_digit_and_pos(String.slice(bank, (pos_one + 1)..String.length(bank)))
     String.to_integer(dig_one <> dig_two)
+  end
+
+  def part2_line_joltage(bank) do
+    # .... 12-1
+    {val, _} =
+      11..0
+      |> Enum.reduce({"", 0}, fn current, {selected_digits, starting_position} ->
+        {digit, new_start} =
+          highest_digit_and_pos(
+            String.slice(bank, starting_position..(String.length(bank) - current - 1))
+          )
+
+        # IO.inspect(digit)
+        {selected_digits <> digit, starting_position + new_start + 1}
+      end)
+
+    # IO.inspect(val)
+    String.to_integer(val)
   end
 
   def highest_digit_and_pos(bank) do
--- day03/test/puzzle_test.exs
+++ day03/test/puzzle_test.exs
@@ -5,24 +5,34 @@ defmodule PuzzleTest do
 
   @sample File.read!(Path.join([__DIR__, "../priv/sample.txt"])) |> String.split("\n", trim: true)
 
-  @tag focus: true
   test "part1 sample" do
     assert Puzzle.part1(@sample) == 357
   end
 
   @tag focus: true
-  test "line_joltage" do
-    assert Puzzle.line_joltage("987654321111111") == 98
-    assert Puzzle.line_joltage("811111111111119") == 89
-    assert Puzzle.line_joltage("234234234234278") == 78
-    assert Puzzle.line_joltage("818181911112111") == 92
+  test "part2 sample" do
+    assert Puzzle.part2(@sample) == 3_121_910_778_619
   end
 
-  @tag focus: true
+  test "part1 line_joltage" do
+    assert Puzzle.part1_line_joltage("987654321111111") == 98
+    assert Puzzle.part1_line_joltage("811111111111119") == 89
+    assert Puzzle.part1_line_joltage("234234234234278") == 78
+    assert Puzzle.part1_line_joltage("818181911112111") == 92
+  end
+
   test "highest_digit_and_pos" do
     assert Puzzle.highest_digit_and_pos("9") == {"9", 0}
     assert Puzzle.highest_digit_and_pos("123456789") == {"9", 8}
     assert Puzzle.highest_digit_and_pos("22") == {"2", 0}
     assert Puzzle.highest_digit_and_pos("23453") == {"5", 3}
+  end
+
+  @tag focus: true
+  test "part2 line_joltage" do
+    assert Puzzle.part2_line_joltage("987654321111111") == 987_654_321_111
+    assert Puzzle.part2_line_joltage("811111111111119") == 811_111_111_119
+    assert Puzzle.part2_line_joltage("234234234234278") == 434_234_234_278
+    assert Puzzle.part2_line_joltage("818181911112111") == 888_911_112_111
   end
 end

366df779526b828cf5a3eca6bb8dd1c602c382ee
Wes Garrison <wes@databasically.com>
2025-12-11 15:13:55 -0600

    6-2 wip

--- day06/lib/puzzle.ex
+++ day06/lib/puzzle.ex
@@ -4,7 +4,10 @@ defmodule Puzzle do
   end
 
   def part2(lines) do
-    lines |> Enum.map(&split_line/1) |> process_problems(&part2_extractor/2) |> Enum.sum()
+    lines
+    |> parse_puzzle
+    |> process_problems(&part2_extractor/2)
+    |> Enum.sum()
   end
 
   def split_line(line) do
@@ -17,20 +20,60 @@ defmodule Puzzle do
     operators
     |> Enum.with_index()
     |> Enum.map(fn {operator, index} ->
-      operands = operand_extractor.(num_lines, index)
+      operands = operand_extractor.(Enum.reverse(num_lines), index)
 
-      case operator do
+      case String.trim(operator) do
         "+" -> Enum.sum(operands)
         "*" -> Enum.product(operands)
       end
     end)
   end
 
-  defp part1_extractor(num_lines, index) do
+  def part1_extractor(num_lines, index) do
     num_lines |> Enum.map(fn nums -> Enum.at(nums, index) |> String.to_integer() end)
   end
 
-  defp part2_extractor(_num_lines, _index) do
-    [1]
+  def part2_extractor(num_lines, index) do
+    num_lines
+    |> Enum.map(fn line -> Enum.at(line, index) |> String.graphemes() end)
+    |> Enum.zip()
+    |> Enum.map(fn x ->
+      Tuple.to_list(x) |> Enum.join("") |> String.trim() |> String.to_integer()
+    end)
+    |> Enum.reverse()
+  end
+
+  def parse_operand_line(line) do
+    line
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.reject(fn {char, _index} -> char == " " end)
+    |> Enum.map(fn {_char, index} -> index end)
+  end
+
+  def parse_puzzle(raw_lines) do
+    line_length = raw_lines |> Enum.map(&String.length/1) |> Enum.max()
+
+    lines =
+      raw_lines
+      |> Enum.reject(fn line -> line == "" end)
+      |> Enum.map(fn line -> String.pad_trailing(line, line_length) end)
+
+    operators = Enum.at(Enum.reverse(lines), 0)
+    operator_positions = parse_operand_line(operators)
+    offsets = operator_positions
+
+    lines
+    |> Enum.map(fn line ->
+      offsets
+      |> Enum.with_index()
+      |> Enum.map(fn {start_offset, index} ->
+        String.slice(
+          line,
+          start_offset,
+          Enum.at(offsets, index + 1, line_length + 1) - start_offset - 1
+        )
+      end)
+    end)
   end
 end
--- day06/test/puzzle_test.exs
+++ day06/test/puzzle_test.exs
@@ -3,27 +3,93 @@ defmodule PuzzleTest do
   use ExUnit.Case, async: true
   doctest Puzzle
 
-  @sample File.read!(Path.join([__DIR__, "../priv/sample.txt"])) |> String.split("\n", trim: true)
+  @sample File.read!(Path.join([__DIR__, "../priv/sample.txt"]))
+          |> String.split("\n", trim: false)
 
-  @tag focus: true
+  # @tag focus: true
   test "part1 sample" do
     assert Puzzle.part1(@sample) == 4_277_556
   end
 
   @tag focus: true
   test "part2 sample" do
-    assert Puzzle.part1(@sample) == 3_263_827
+    assert Puzzle.part2(@sample) == 3_263_827
   end
 
   test "split_line" do
     assert Puzzle.split_line("1  2   3 4 5 6             ") == ["1", "2", "3", "4", "5", "6"]
   end
 
+  # @tag focus: true
+  test "process problems (simple)" do
+    assert Puzzle.process_problems([["1"], ["+"]], &Puzzle.part1_extractor/2) == [1]
+    assert Puzzle.process_problems([["1"], ["*"]], &Puzzle.part1_extractor/2) == [1]
+    assert Puzzle.process_problems([["1"], ["1"], ["+"]], &Puzzle.part1_extractor/2) == [2]
+    assert Puzzle.process_problems([["1"], ["1"], ["*"]], &Puzzle.part1_extractor/2) == [1]
+  end
+
+  @tag xfocus: true
+  test "process problems (parts)" do
+    assert Puzzle.process_problems([["1"], ["+"]], &Puzzle.part2_extractor/2) == [1]
+    assert Puzzle.process_problems([["1"], ["*"]], &Puzzle.part2_extractor/2) == [1]
+    assert Puzzle.process_problems([["1"], ["1"], ["+"]], &Puzzle.part2_extractor/2) == [2]
+    assert Puzzle.process_problems([["1"], ["1"], ["*"]], &Puzzle.part2_extractor/2) == [1]
+  end
+
+  test "parse operand line" do
+    assert Puzzle.parse_operand_line("*   +   *   +") == [0, 4, 8, 12]
+    assert Puzzle.parse_operand_line("* + * +") == [0, 2, 4, 6]
+  end
+
   @tag focus: true
-  test "process promblems (simple)" do
-    assert Puzzle.process_problems([["1"], ["+"]]) == [1]
-    assert Puzzle.process_problems([["1"], ["*"]]) == [1]
-    assert Puzzle.process_problems([["1"], ["1"], ["+"]]) == [2]
-    assert Puzzle.process_problems([["1"], ["1"], ["*"]]) == [1]
+  test "parse sample input" do
+    assert Puzzle.parse_puzzle(@sample) == [
+             ["123", "328", " 51", "64 "],
+             [" 45", "64 ", "387", "23 "],
+             ["  6", "98 ", "215", "314"],
+             ["*  ", "+  ", "*  ", "+  "]
+           ]
+  end
+
+  @tag focus: true
+  test "part2 extractor" do
+    assert Puzzle.part2_extractor(
+             [
+               ["123", "328", " 51", "64 "],
+               [" 45", "64 ", "387", "23 "],
+               ["  6", "98 ", "215", "314"]
+             ],
+             0
+           ) == [356, 24, 1]
+
+    assert Puzzle.part2_extractor(
+             [
+               ["123", "328", " 51", "64 "],
+               [" 45", "64 ", "387", "23 "],
+               ["  6", "98 ", "215", "314"]
+             ],
+             1
+           ) ==
+             [8, 248, 369]
+
+    assert Puzzle.part2_extractor(
+             [
+               ["123", "328", " 51", "64 "],
+               [" 45", "64 ", "387", "23 "],
+               ["  6", "98 ", "215", "314"]
+             ],
+             2
+           ) ==
+             [175, 581, 32]
+
+    assert Puzzle.part2_extractor(
+             [
+               ["123", "328", " 51", "64 "],
+               [" 45", "64 ", "387", "23 "],
+               ["  6", "98 ", "215", "314"]
+             ],
+             3
+           ) ==
+             [4, 431, 623]
   end
 end
